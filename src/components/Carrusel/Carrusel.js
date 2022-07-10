19aad4650eaa9eb39485d20a5e7e22e6bf1eba5b
maxt0d0nt <maxbox21@hotmail.com>
2022-07-10 22:32:33 +0200

    carrousel

--- src/components/Carrusel/Carrusel.js
+++ src/components/Carrusel/Carrusel.js
@@ -12,6 +12,33 @@ export const Carrusel = () => {
   console.log('next')
   
   };
+
+  let slides = [
+    {
+      title: "Route Plannning",
+      description: "Intelligent route planning and automatic re-mapping to avoid obstacles.",
+      src: vector1,
+      id: 1
+    },
+    {
+      title: "Autonomously clean floors",
+      description: "Ability to autonomously clean floors following pre-mapped routes.",
+      src: vector2,
+      id: 2
+    },
+    {
+      title: "Without human supervision.",
+      description: "Execution of tasks without human supervision.",
+      src: vector3,
+      id: 3
+    },
+    {
+      title: "Autonomously clean floors",
+      description: "Ability to autonomously clean floors following pre-mapped routes.",
+      src: vector2,
+      id: 2
+    }
+  ];
   
   const settings = {
     infinite: true,
@@ -28,69 +55,24 @@ export const Carrusel = () => {
   return (
     <div className='Carrusel'>
       <Slider {...settings}>
-      <div className='card-container'>
-        <div className='card'>
-          <div className='card_img'>
-            <img className="vector" src={vector1} alt="UR_vector1" id='vector1'/>
-          </div>
-          <div className='card_title'>
-            <p>Route planning</p>
-          </div>
-          <div className='card_description'>
-            <p>Intelligent route planning and automatic re-mapping to avoid obstacles.</p>
-          </div>
-            <button className='button_card' onClick={() => NextArrow()}>NEXT</button>
-            
-        </div>
-      </div>
-
-      <div className='card-container'>
-        <div className='card'>
-          <div className='card_img'>
-            <img className="vector" src={vector2} alt="UR_vector1" id='vector2'/>
-
-          </div>
-          <div className='card_title'>
-            <p>Autonomously clean floors</p>
-          </div>
-          <div className='card_description'>
-            <p>Ability to autonomously clean floors following pre-mapped routes.</p>
-          </div>
-          <button className='button_card' type='button'>NEXT</button>
-        </div>
-      </div>
-
-      <div className='card-container'>
-        <div className='card'>
-          <div className='card_img'>
-            <img className="vector" src={vector3} alt="UR_vector1" id='vector3'/>
-          </div>
-          <div className='card_title'>
-            <p>Without human supervision.</p>
-          </div>
-          <div className='card_description'>
-            <p>Execution of tasks without human supervision.</p>
-          </div>
-          <button className='button_card' type='button'>NEXT</button>
-        </div>
-      </div>
-
-      <div className='card-container'>
-        <div className='card'>
-          <div className='card_img'>
-            <img className="vector" src={vector2} alt="UR_vector1" id='vector2'/>
-          </div>
-          <div className='card_title'>
-            <p>Autonomously clean floors</p>
-          </div>
-          <div className='card_description'>
-            <p>Ability to autonomously clean floors following pre-mapped routes.</p>
-          </div>
-          <button className='button_card' type='button'>NEXT</button>
-        </div>
-      </div>
-
-      
+        {slides.map((slide, i) => {
+          return(
+            <div className='card-container'>
+              <div className='card'>
+                <div className='card_img'>
+                  <img className="vector" src={slide.src} alt={"UR_vector" + Number(i) + 1} id={"vector" + slide.id}/>
+                </div>
+                <div className='card_title'>
+                  <p>{slide.title}</p>
+                </div>
+                <div className='card_description'>
+                  <p>{slide.description}</p>
+                </div>
+                <button className='button_card' type='button'>NEXT</button>
+              </div>
+            </div>          
+          )        
+        })}      
       </Slider>
     </div>
   )
